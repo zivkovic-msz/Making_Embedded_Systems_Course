44dfcffd5e96c7c7f086de0545b198abf7f369a8
Mihailo Zivkovic <zivkovic.msz@gmail.com>
2022-04-30 11:09:56 -0400

    Revert "Revert "Ported CLI template for STM32WB EVB""
    This reverts commit f1a92023cbae80130ed98a6183ed84f474a97d66.

---
+++ Assignments/Week_5/mes_lesson_6_add_a_cli/Debug/Core/Src/consoleCommands.d
@@ -0,0 +1,8 @@
+Core/Src/consoleCommands.o: ../Core/Src/consoleCommands.c \
+ ../Core/Inc/consoleCommands.h ../Core/Inc/console.h \
+ ../Core/Inc/console.h ../Core/Inc/consoleIo.h ../Core/Inc/version.h
+../Core/Inc/consoleCommands.h:
+../Core/Inc/console.h:
+../Core/Inc/console.h:
+../Core/Inc/consoleIo.h:
+../Core/Inc/version.h:
