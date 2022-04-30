43e3ee151c2631a5a2f9a5ff4da505ff526128c0
Mihailo Zivkovic <zivkovic.msz@gmail.com>
2022-04-30 11:07:34 -0400

    Ported CLI template for STM32WB EVB

---
+++ Assignments/Week_5/mes_lesson_6_add_a_cli/Debug/Core/Src/console.d
@@ -0,0 +1,7 @@
+Core/Src/console.o: ../Core/Src/console.c ../Core/Inc/console.h \
+ ../Core/Inc/consoleIo.h ../Core/Inc/consoleCommands.h \
+ ../Core/Inc/console.h
+../Core/Inc/console.h:
+../Core/Inc/consoleIo.h:
+../Core/Inc/consoleCommands.h:
+../Core/Inc/console.h:
